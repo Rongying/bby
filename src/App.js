9adaee2daf8775b647200f86509f35453e1e4acb
Rongying Zhou <fee.zry@gmail.com>
2017-08-30 16:04:50 +0800

    init based on react-native-app script

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BBY from './app/app';
+ 
+export default class App extends React.Component {
+  render() {
+  let a = 'sss';
+    return (
+      <BBY />
+    );
+  }
+}
