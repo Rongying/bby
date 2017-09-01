91ad7e4ff6f34ee70ce318890193a004390af089
Rongying Zhou <fee.zry@gmail.com>
2017-09-01 19:22:46 +0800

    add data folder

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { RkButton } from 'react-native-ui-kitten';
+import {
+  StackNavigator
+} from 'react-navigation';
 
 export default class BBY extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Changes you make will automatically reload?</Text>
+        <Text>Changes you make will tomatically reload?</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <RkButton>Click me!</RkButton>
       </View>
     );
   }
---
+++ src/app/data/raw/exercises.js
@@ -0,0 +1,3 @@
+const exercises = [];
+
+export default exercises;
